8269dd2e55c96023aaa86a247ee73923543cbd95
tstassin <tstassin@gmail.com>
2019-08-01 11:29:49 +0200

    update package name, version and sample

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const ponto = require('./modules/ponto')
 require('dotenv').config()
 
 async function main() {
-    var myponto = new ponto()
-    res = await myponto.configure(process.env.PONTO_API_KEY)
-    let accounts = await myponto.listAccounts()
-    accounts.data.map( account => console.log(account.attributes.reference + " / " + account.attributes.currentBalance + account.attributes.currency) )
-    let anAccount = await myponto.getAccount(accounts.data[0].id)
-    let transactionsFromThisAccount = await myponto.listTransactions(anAccount.data.id, {limit: 3})
-    console.log (transactionsFromThisAccount)
-    let nextTransactions = await myponto.listTransactions(anAccount.data.id, 
-        {limit:3, after: transactionsFromThisAccount.meta.paging.after})
-    console.log(nextTransactions)
+    try {
+        var myponto = new ponto()
+        await myponto.configure(process.env.PONTO_API_KEY)
+        let accounts = await myponto.listAccounts()
+        accounts.data.map(account => console.log(account.attributes.reference + " / " + account.attributes.currentBalance + account.attributes.currency))
+        let anAccount = await myponto.getAccount(accounts.data[0].id)
+        let transactionsFromThisAccount = await myponto.listTransactions(anAccount.data.id, { limit: 3 })
+        console.log(transactionsFromThisAccount)
+        let nextTransactions = await myponto.listTransactions(anAccount.data.id, { limit: 3, after: transactionsFromThisAccount.meta.paging.after })
+        console.log(nextTransactions)
+    } catch (e) {
+        console.error(e.statusCode, e.statusMessage)
+    }
 }
 
 main()
